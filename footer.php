28a59367032fda2a7b04fb590afca75fded08373
Donny <donw_92@live.nl>
2017-01-10 09:45:36 +0100

    New design for the homepage.
    Updated layout and files for the site.

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer class="footer footer-style_1">
+    <div class="container">
+        <div class="footer-top">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div class="logo-footer">
+                        <a href="#">
+                            <img src="assets/images/logo-light.png" alt="img">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-7">
+                    <div class="menu-footer footer-center">
+                        <ul class="menu">
+                            <li class="menu-item"><a href="about-us.html">about</a></li>
+                            <li class="menu-item"><a href="blog.html">blog</a></li>
+                            <li class="menu-item"><a href="shop.html">shop</a></li>
+                            <li class="menu-item"><a href="contact.html">contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="social-footer footer-right">
+                        <ul class="socials">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Back To Top -->
+    <a class="backtotop" href="#">
+        <span class="icon-top fa fa-angle-up"></span> Top
+    </a>
+    <!-- End Back To Top -->
+</footer>
--- index.php
+++ index.php
@@ -1,1 +1,193 @@
-git 
+<!-- the header untill the main nav -->
+<?php include 'header-top.php'; ?>
+<!-- end the header untill the main nav -->
+<!-- Main Nav Menu -->
+<nav id="primary-navigation" class="site-navigation nav-show">
+    <div id="main-menu" class="main-nav main-menu">
+        <ul class="menu-nav">
+            <li class="menu-item active">
+                <a href="index.php">Home</a>
+            </li>
+            <li class="menu-item">
+                <a href="amsterdam.php">Amsterdam</a>
+            </li>
+            <li class="menu-item"><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+<!-- End Main Nav Menu -->
+<!-- Rest of the header -->
+<?php include 'header.php'; ?>
+<!-- end Rest of the header -->
+<!-- Header Banner -->
+<div class="header-banner banner-slide">
+    <div class="xshop-owl-carousel banner-wrap rev_slider_wrapper" data-autoPlay="yes" data-navControl="yes" data-number="1">
+        <div class="item-slide">
+            <figure>
+                <img src="assets/images/slide-home4/slide7.png" alt="img">
+            </figure>
+            <div class="slide-content content-slide-v4 text-center">
+                <h6 class="subtitle-banner-smaller">Check out one of the most famous musea in Amsterdam</h6>
+                <h2 class="title-banner-smaller">Rijksmuseum Amsterdam</h2>
+                <a href="#" class="button-slide button-slide-light">Get your tickets now!</a>
+            </div>
+        </div>
+        <div class="item-slide">
+            <figure>
+                <img src="assets/images/slide-home4/slide8.png" alt="img">
+            </figure>
+            <div class="slide-content content-slide-v4 text-center">
+                <h6 class="subtitle-banner-smaller">Beer is all you need right? see how they make it in</h6>
+                <h2 class="title-banner-smaller">Heineken Experience</h2>
+                <a href="#" class="button-slide button-slide-light">Get your tickets now!</a>
+            </div>
+        </div>
+        <div class="item-slide">
+            <figure>
+                <img src="assets/images/slide-home4/slide9.png" alt="img">
+            </figure>
+            <div class="slide-content content-slide-v4 text-center">
+                <h6 class="subtitle-banner-smaller">Wanne ride the canals of Amsterdam</h6>
+                <h2 class="title-banner-smaller">Amsterdam Canal Cruises</h2>
+                <a href="#" class="button-slide button-slide-light">Get your tickets now!</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Header Banner -->
+</header>
+    <div class="main-content main-content-box">
+        <section class="padding-top75 padding-bottom60">
+            <div classs="row">
+                <div class="col-sm-6 nopadding-right">
+                    <!-- Sevice Box -->
+                    <div class="sevice-box-wrap sevice-style_1">
+                        <figure><img src="assets/images/img4-h4.jpg" alt=""></figure>
+                        <div class="sevice-box-content">
+                            <h2 class="sevice-title"><img src="assets/images/lovers-logo.png" /></h2>
+                            <div class="sevide-desc">Experience Amsterdam from a unique perspective with the one hour Amsterdam Day Canal Cruise!</div>
+                            <a href="shop.html" class="sevice-button">Get Your Ticket</a>
+                        </div>
+                    </div>
+                    <!-- End Sevice Box -->
+                </div>
+                <div class="col-sm-6 nopadding-left">
+                    <div class="img-signle">
+                        <figure>
+                            <img src="assets/images/lovers-image.png" alt="">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="row">
+                <div class="col-sm-6 nopadding-right" style="padding-right:0;">
+                    <div class="img-signle">
+                        <figure>
+                            <img src="assets/images/canal-ride.png" alt="">
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-sm-6 nopadding-left">
+                    <!-- Sevice Box -->
+                    <div class="sevice-box-wrap sevice-style_1">
+                        <figure><img src="assets/images/img4-h4.jpg" alt=""></figure>
+                        <div class="sevice-box-content">
+                            <h2 class="sevice-title"><img src="assets/images/canal-logo.png" /></h2>
+                            <div class="sevide-desc">Een leuke activiteit die niet mag ontbreken tijdens een dagje uit in Amsterdam. Vaar door de Amsterdamse grachten en maak kennis met alle bezienswaardigheden van het Unesco Werelderfgoed. Ontdek op een van de boottochten van Canal waarom Amsterdam het mooist is vanaf het water.</div>
+                            <a href="shop.html" class="sevice-button">Get Your Ticket</a>
+                        </div>
+                    </div>
+                    <!-- End Sevice Box -->
+                </div>
+                    <!-- End Product Signle Item -->
+                </div>
+            </div>
+        </section>
+        <section class="padding-bottom60 padding-top60">
+            <div class="row">
+                <div class="col-sm-6 nopadding-right" style="padding-right:0;">
+                    <!-- Product Signle Item -->
+                    <div class="product-signle-item">
+                        <div class="product-media">
+                            <div class="product-images-wrap">
+                                <figure class="product-first-figure">
+                                    <a href="#">
+                                        <img alt="img" src="assets/images/img5-h4.jpg">
+                                    </a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="product-info">
+                            <h3 class="product-name"><a href="#">Rust Shirt With Half Sleeves</a></h3>
+                            <span class="price">$75.00</span>
+                        </div>
+                    </div>
+                    <!-- End Product Signle Item -->
+                </div>
+                <div class="col-sm-6 nopadding-left" style="padding-left: 0;">
+                    <!-- Sevice Box -->
+                    <div class="sevice-box-wrap sevice-style_1">
+                        <figure><img src="assets/images/img6-h4.jpg" alt=""></figure>
+                        <div class="sevice-box-content">
+                            <h2 class="sevice-title">Toni & Guy Men Styling</h2>
+                            <div class="sevide-desc">
+                                Inspiration can come from anywhere; in this instance a jacket gifted to us by a close friend caught our imagination. Not only was it a badass jacket but it wasused as a prop on set of the Brad Pitt film, Fury. This fuelled an entire military themed collection with a huge array...
+                            </div>
+                            <a href="shop.html" class="sevice-button">read more</a>
+                        </div>
+                    </div>
+                    <!-- End Sevice Box -->
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="row">
+                <div class="col-sm-6 nopadding-right" style="padding-right:0;" >
+                    <div class="img-signle">
+                        <figure>
+                            <img src="assets/images/img8-h4.jpg" alt="">
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-sm-6 nopadding-left" style="padding-left: 0;">
+                    <!-- Product Signle Item -->
+                    <div class="product-signle-item">
+                        <div class="product-media">
+                            <div class="product-images-wrap">
+                                <figure class="product-first-figure">
+                                    <a href="#">
+                                        <img alt="img" src="assets/images/img7-h4.jpg">
+                                    </a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="product-info">
+                            <h3 class="product-name"><a href="#">Rust Shirt With Half Sleeves</a></h3>
+                            <span class="price">$75.00</span>
+                        </div>
+                    </div>
+                    <!-- End Product Signle Item -->
+                </div>
+            </div>
+        </section>
+    </div>
+    <?php include 'footer.php'; ?>
+
+    <script type='text/javascript' src='assets/js/jquery.min.js'></script>
+    <script type='text/javascript' src='assets/js/owl.carousel.min.js'></script>
+    <script type='text/javascript' src='assets/js/bootstrap.min.js'></script>
+    <script type='text/javascript' src='assets/js/slick.js'></script>
+    <script type='text/javascript' src='assets/js/wow.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery.mmenu.all.min.js'></script>
+    <script type='text/javascript' src='assets/js/lightbox.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery.scrollbar.js'></script>
+    <script type='text/javascript' src='assets/js/chosen.jquery.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery-ui.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery.bxslider.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery.countdown.min.js'></script>
+    <script type='text/javascript' src='assets/js/frontend.js'></script>
+    <script type='text/javascript' src='assets/js/frontend-plugin.js'></script>
+</body>
+</html>
